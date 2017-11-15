109e4b61ffdb6feabfe3f7f4e5f2c881b3c0ca07
vloncaric <vloncaric@etfos.hr>
2017-11-15 10:10:06 +0100

    Add courses.txt parsing

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ from classes.course import Course
 if __name__== "__main__":
     print "Tooooo"
 
+with open ("courses.txt", "r") as f:
+    lines = f.readlines()
+    
+    courses = []
+    data = []
+    for line in lines :
+        if not line.startswith ("#"):
+            data.append (line.strip() )
+    for line in data:
+        x = line.split('|')
+        if x[0].strip() == "Course":
+            name = x[1].strip()
+            code = x[2].strip()
+            courses.append( Course (name, code) )
+        elif x[0].strip() == "Running":
+            year = int (x[1])
+            courses [-1].add_running(year)
+    
+    for course in courses:
+        print course
+           
+
+   
